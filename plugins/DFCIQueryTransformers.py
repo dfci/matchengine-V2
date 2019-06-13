c0d80a7ea4bd972142261aa39d664c649b98e267
Eric Marriott <marriott@bcb.dfci.harvard.edu>
2019-06-13 15:49:24 -0400

    plugin architechture

---
+++ plugins/DFCIQueryTransformers.py
@@ -0,0 +1,120 @@
+import datetime
+
+from dateutil.relativedelta import relativedelta
+
+from query_transform import QueryTransformerContainer
+
+
+class DFCITransformers(QueryTransformerContainer):
+    def age_range_to_date_query(self, **kwargs):
+        sample_key = kwargs['sample_key']
+        trial_value = kwargs['trial_value']
+        operator_map = {
+            "==": "$eq",
+            "<=": "$gte",
+            ">=": "$lte",
+            ">": "$lt",
+            "<": "$gt"
+        }
+        # funky logic is because 1 month curation is curated as "0.083" (1/12 a year)
+        operator = ''.join([i for i in trial_value if not i.isdigit() and i != '.'])
+        numeric = "".join([i for i in trial_value if i.isdigit() or i == '.'])
+        split_time = numeric.split('.')
+        years = int(split_time[0] if split_time[0].isdigit() else 0)
+        months_fraction = float(split_time[1]) if len(split_time) > 1 else 0
+        months = int(months_fraction * 12)
+        current_date = datetime.date.today()
+        query_date = current_date - relativedelta(years=years, months=months)
+        query_datetime = datetime.datetime(query_date.year, query_date.month, query_date.day, 0, 0, 0, 0)
+        return {sample_key: {operator_map[operator]: query_datetime}}, False
+
+    def bool_from_text(self, **kwargs):
+        trial_value = kwargs['trial_value']
+        sample_key = kwargs['sample_key']
+        if trial_value.upper() == 'TRUE':
+            return {sample_key: True}, False
+        elif trial_value.upper() == 'FALSE':
+            return {sample_key: False}, False
+
+    def cnv_map(self, **kwargs):
+        # Heterozygous deletion,
+        # Gain,
+        # Homozygous deletion,
+        # High level amplification,
+        # Neu
+
+        trial_value = kwargs['trial_value']
+        sample_key = kwargs['sample_key']
+        cnv_map = {
+            "High Amplification": "High level amplification",
+            "Homozygous Deletion": "Homozygous deletion",
+            'Low Amplification': 'Gain',
+            'Heterozygous Deletion': 'Heterozygous deletion'
+
+        }
+
+        trial_value, negate = self._.transform.is_negate(trial_value)
+        if trial_value in cnv_map:
+            return {sample_key: cnv_map[trial_value]}, negate
+        else:
+            return {sample_key: trial_value}, negate
+
+    def variant_category_map(self, **kwargs):
+        trial_value = kwargs['trial_value']
+        sample_key = kwargs['sample_key']
+        variant_category_map = {
+            "Copy Number Variation": "CNV"
+        }
+
+        trial_value, negate = self._.transform.is_negate(trial_value)
+
+        # if a curation calls for a Structural Variant, search the free text in the genomic document under
+        # STRUCTURAL_VARIANT_COMMENT for mention of the TRUE_HUGO_SYMBOL
+        if trial_value == 'Structural Variation':
+            return {'STRUCTURAL_VARIANT_COMMENT': None}, negate
+        elif trial_value in variant_category_map:
+            return {sample_key: variant_category_map[trial_value]}, negate
+        else:
+            return {sample_key: trial_value.upper()}, negate
+
+    def wildcard_regex(self, **kwargs):
+        """
+        When trial curation criteria include a wildcard prefix (e.g. WILDCARD_PROTEIN_CHANGE), a genomic query must
+        use a $regex to search for all genomic documents which match the protein prefix.
+
+        E.g.
+        Trial curation match clause:
+        | genomic:
+        |    wildcard_protein_change: p.R132
+
+        Patient genomic data:
+        |    true_protein_change: p.R132H
+
+        The above should match in a mongo query.
+        """
+        trial_value = kwargs['trial_value']
+
+        # By convention, all protein changes being with "p."
+        if not trial_value.startswith('p.'):
+            trial_value = 'p.' + trial_value
+
+        trial_value, negate = self._.transform.is_negate(trial_value)
+        trial_value = '^%s[A-Z]' % trial_value
+        return {kwargs['sample_key']: {'$regex': trial_value}}, negate
+
+    def mmr_ms_map(self, **kwargs):
+        mmr_map = {
+            'MMR-Proficient': 'Proficient (MMR-P / MSS)',
+            'MMR-Deficient': 'Deficient (MMR-D / MSI-H)',
+            'MSI-H': 'Deficient (MMR-D / MSI-H)',
+            'MSI-L': 'Proficient (MMR-P / MSS)',
+            'MSS': 'Proficient (MMR-P / MSS)'
+        }
+        trial_value = kwargs['trial_value']
+        trial_value, negate = self._.transform.is_negate(trial_value)
+        sample_key = kwargs['sample_key']
+        sample_value = mmr_map[trial_value]
+        return {sample_key: sample_value}, negate
+
+
+__export__ = ["DFCITransformers"]
---
+++ query_transform.py
@@ -0,0 +1,71 @@
+import json
+from types import MethodType
+from typing import Type, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from match_criteria_transform import MatchCriteriaTransform
+else:
+    MatchCriteriaTransform = object()
+
+
+class QueryTransformerContainer(object):
+    if TYPE_CHECKING:
+        _: MatchCriteriaTransform
+
+
+def is_negate(trial_value):
+    """
+    Example: !EGFR => (True, EGFR)
+
+    :param trial_value:
+    :return:
+    """
+    negate = True if isinstance(trial_value, str) and trial_value[0] == '!' else False
+    trial_value = trial_value[1::] if negate else trial_value
+    return trial_value, negate
+
+
+def attach_transformers_to_match_criteria_transform(match_criteria_transform: MatchCriteriaTransform,
+                                                    query_transformer_container: Type[QueryTransformerContainer]):
+    for attr in dir(query_transformer_container):
+        if not attr.startswith('_'):
+            method = getattr(query_transformer_container, attr)
+            setattr(match_criteria_transform.query_transformers,
+                    attr,
+                    MethodType(method, match_criteria_transform.query_transformers))
+
+
+class BaseTransformers(QueryTransformerContainer):
+
+    def nomap(self, **kwargs):
+        trial_path = kwargs['trial_path']
+        trial_key = kwargs['trial_key']
+        trial_value = kwargs['trial_value']
+        sample_key = kwargs['sample_key']
+        trial_value, negate = is_negate(trial_value)
+        return {sample_key: trial_value}, negate
+
+    def external_file_mapping(self, **kwargs):
+        trial_value = kwargs['trial_value']
+        sample_key = kwargs['sample_key']
+        file = kwargs['file']
+        if file not in self._.resources:
+            with open(file) as file_handle:
+                self._.resources[file] = json.load(file_handle)
+        resource = self._.resources[file]
+        trial_value, negate = is_negate(trial_value)
+        match_value = resource.setdefault(trial_value, trial_value)  # TODO: fix
+        if isinstance(match_value, list):
+            return {sample_key: {"$in": sorted(match_value)}}, negate
+        else:
+            return {sample_key: match_value}, negate
+
+    def to_upper(self, **kwargs):
+        trial_value = kwargs['trial_value']
+        sample_key = kwargs['sample_key']
+        trial_value, negate = is_negate(trial_value)
+        return {sample_key: trial_value.upper()}, negate
+
+
+__export__ = ["BaseTransformers"]
+__shared__ = ["is_negate"]
