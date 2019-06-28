9693c64eee7ae5e5f298d1c0402d83893e17b462
esiegel <esiegel@jimmy.harvard.edu>
2019-06-28 16:47:56 -0400

    WIP

--- tests/test_matchengine_unit.py
+++ tests/test_matchengine_unit.py
@@ -1,9 +1,9 @@
+import glob
 import json
 from unittest import TestCase
-
-from TrialMatchDocumentCreator import TrialMatchDocumentCreator
 from match_criteria_transform import MatchCriteriaTransform
-from matchengine import MatchEngine
+from matchengine import MatchEngine, log
+from matchengine_types import MatchClauseData, ParentPath, MatchClauseLevel
 
 
 class TestMatchEngine(TestCase):
@@ -26,63 +26,44 @@ class TestMatchEngine(TestCase):
         assert hasattr(self.me, 'create_trial_matches')
         assert id(self.me.create_trial_matches) != old_create_trial_matches
 
-
-    def test__async_init(self):
-        self.fail()
-
-    def test__execute_clinical_queries(self):
-        self.fail()
-
-    def test__execute_genomic_queries(self):
-        self.fail()
-
-    def test__run_query(self):
-        self.fail()
-
-    def test__queue_worker(self):
-        self.fail()
-
     def test_extract_match_clauses_from_trial(self):
-        self.fail()
+        self.me.trials = dict()
+        self.me.match_on_closed = False
+        with open('./tests/data/trials/11-111.json') as f:
+            data = json.load(f)
+            match_clause = data['treatment_list']['step'][0]['arm'][0]['match'][0]
+            self.me.trials['11-111'] = data
+
+        extracted = next(self.me.extract_match_clauses_from_trial('11-111'))
+        assert extracted.match_clause[0]['and'] == match_clause['and']
+        assert extracted.parent_path == ('treatment_list', 'step', 0, 'arm', 0, 'match')
+        assert extracted.match_clause_level == 'arm'
+        assert extracted.code == 'EXP 3'
 
     def test_create_match_tree(self):
-        self.fail()
+        self.me.trials = dict()
+        self.me.visualize_match_paths = False
+        for file in glob.glob('./tests/data/ctml_boolean_cases/*.json'):
+            with open(file) as f:
+                data = json.load(f)
+                trial = [data]
+                self.me.trials[file] = trial
+
+        for trial in self.me.trials:
+            me_trial = self.me.trials[trial]
+            match_tree = self.me.create_match_tree(MatchClauseData(match_clause=me_trial,
+                                                                   internal_id='123',
+                                                                   code='456',
+                                                                   coordinating_center='The Death Star',
+                                                                   status='Open to Accrual',
+                                                                   parent_path=ParentPath(()),
+                                                                   match_clause_level=MatchClauseLevel('arm'),
+                                                                   match_clause_additional_attributes={},
+                                                                   protocol_no='12-345'))
+            pass
 
     def test_get_match_paths(self):
         self.fail()
 
     def test_translate_match_path(self):
-        self.fail()
-
-    def test_update_matches_for_protocol_number(self):
-        self.fail()
-
-    def test_update_all_matches(self):
-        self.fail()
-
-    def test__async_update_matches_by_protocol_no(self):
-        self.fail()
-
-    def test_get_matches_for_all_trials(self):
-        self.fail()
-
-    def test_get_matches_for_trial(self):
-        self.fail()
-
-    def test__async_get_matches_for_trial(self):
-        self.fail()
-
-    def test_get_clinical_ids_from_sample_ids(self):
-        self.fail()
-
-    def test_get_trials(self):
-        self.fail()
-
-    def test__perform_db_call(self):
-        self.fail()
-
-    def test_get_sort_order(self):
-        self.fail()
-
-    def test_create_trial_matches(self):
         self.fail()
