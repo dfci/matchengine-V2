e713e6004a08766b249875035ef684e31ef9359e
Eric Marriott <marriott@bcb.dfci.harvard.edu>
2019-07-12 14:21:38 -0400

    static datetime.datetime.now and datetime.date.today for integration testing

--- tests/test_matchengine_integration.py
+++ tests/test_matchengine_integration.py
@@ -1,6 +1,23 @@
 from unittest import TestCase
+import gc
 
 from matchengine import MatchEngine
+
+import datetime
+
+
+# old_datetime_datetime_now = datetime.datetime.now
+class StaticDatetime(datetime.datetime):
+    @classmethod
+    def now(cls, **kwargs):
+        return datetime.datetime(2000, 7, 12, 9, 47, 40, 303620)
+
+
+# old_datetime_datetime_now = datetime.datetime.now
+class StaticDate(datetime.date):
+    @classmethod
+    def today(cls):
+        return datetime.date(2000, 7, 12)
 
 
 class IntegrationTestMatchengine(TestCase):
@@ -14,9 +31,20 @@ class IntegrationTestMatchengine(TestCase):
             plugin_dir='plugins',
             use_run_log=False
         )
+        self.me.match_criteria_transform.transform.static_date = StaticDate
+        for override_class in [StaticDate, StaticDatetime]:
+            for base_class in override_class.__bases__:
+                for referrer in gc.get_referrers(base_class):
+                    if getattr(referrer, '__hash__', None) is None:
+                        if isinstance(referrer, dict):
+                            for k in list(referrer.keys()):
+                                if referrer[k] is base_class:
+                                    referrer[k] = override_class
         pass
 
     def test_basic(self):
         assert True
 
     pass
+
+# self.me.match_criteria_transform.query_transformers.age_range_to_date_query(**{'sample_key': 'test', 'trial_value': '>=18'})
