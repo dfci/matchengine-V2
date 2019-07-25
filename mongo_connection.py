ce83247b7bb35645d416ff95bcfdbbf0ba816a0c
Eric Marriott <marriott@bcb.dfci.harvard.edu>
2019-07-25 12:12:08 -0400

    mongo connection allow blank database without user/auth

--- mongo_connection.py
+++ mongo_connection.py
@@ -29,11 +29,11 @@ class DefaultDBSecrets(DBSecrets):
         return Secrets(HOST=self._secrets["MONGO_HOST"],
                        PORT=self._secrets["MONGO_PORT"],
                        DB=self._secrets["MONGO_DBNAME"],
-                       AUTH_DB=self._secrets["MONGO_AUTH_SOURCE"],
-                       RO_USERNAME=self._secrets["MONGO_RO_USERNAME"],
-                       RO_PASSWORD=self._secrets["MONGO_RO_PASSWORD"],
-                       RW_USERNAME=self._secrets["MONGO_USERNAME"],
-                       RW_PASSWORD=self._secrets["MONGO_PASSWORD"],
+                       AUTH_DB=self._secrets.get("MONGO_AUTH_SOURCE", False),
+                       RO_USERNAME=self._secrets.get("MONGO_RO_USERNAME", False),
+                       RO_PASSWORD=self._secrets.get("MONGO_RO_PASSWORD", False),
+                       RW_USERNAME=self._secrets.get("MONGO_USERNAME", False),
+                       RW_PASSWORD=self._secrets.get("MONGO_PASSWORD", False),
                        REPLICASET=self._secrets.get("MONGO_REPLICASET", False),
                        MAX_POOL_SIZE=self._secrets.get("MONGO_MAX_POOL_SIZE", False))
 
@@ -51,7 +51,6 @@ class MongoDBConnection(object):
 
         Override SECRETS_JSON values if arguments are passed via CLI
         :param read_only:
-        :param uri:
         :param db:
         """
         self.read_only = read_only
@@ -71,8 +70,12 @@ class MongoDBConnection(object):
             uri_params.append(f"replicaSet={self.secrets.REPLICASET}")
         if self.secrets.MAX_POOL_SIZE:
             uri_params.append(f"maxPoolSize={self.secrets.MAX_POOL_SIZE}")
+        username_password_param = (f"{username if username else str()}"
+                                   f"{':' if username and password else str()}"
+                                   f"{password if password else str()}"
+                                   f"{'@' if username or password else str()}")
 
-        uri = (f"mongodb://{username}:{password}@{self.secrets.HOST}:{self.secrets.PORT}/{self.db}"
+        uri = (f"mongodb://{username_password_param}{self.secrets.HOST}:{self.secrets.PORT}/{self.db}"
                f"{'?' if uri_params else str()}{'&'.join(uri_params)}")
         if self.async_init:
             self.client = motor.motor_asyncio.AsyncIOMotorClient(uri)
