0a3091c509d4deea761ff3cfd555b35677af913b
Eric Marriott <marriott@ds.dfci.harvard.edu>
2019-08-03 19:51:03 -0400

    logging

--- matchengine/engine.py
+++ matchengine/engine.py
@@ -354,6 +354,7 @@ class MatchEngine(object):
                                                  match_path,
                                                  query,
                                                  clinical_ids_to_run))
+        log.info(f"Submitted {self._task_q.qsize()} QueryTasks to queue")
         await self._task_q.join()
         logging.info(f"Total results: {len(self._matches[protocol_no])}")
         return self._matches[protocol_no]
