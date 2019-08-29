c181bfdce390d7643ed6434134ca8e788e9c2c1e
Eric Marriott <marriott@ds.dfci.harvard.edu>
2019-08-28 22:33:07 -0400

    use uvloop

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(
         "Pandas>=0.25.0",
         "pymongo==3.8.0",
         "networkx==2.3",
-        "motor==2.0.0"
+        "motor==2.0.0",
+        "uvloop>=0.13.0"
     ],
     include_package_data=True
 )
