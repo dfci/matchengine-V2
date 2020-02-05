df85d13559491bf07d39c2998b12e3a8d72aea32
emarriott <Limpitl0g>
2020-02-05 10:56:45 -0500

    allow greater versions in requirements

--- setup.py
+++ setup.py
@@ -19,12 +19,12 @@ setup(
     },
     install_requires=[
         "bson>=0.5.8",
-        "python-dateutil==2.8.0",
-        "PyYAML==5.1",
+        "python-dateutil>=2.8.0",
+        "PyYAML>=5.1",
         "Pandas>=0.25.0",
-        "pymongo==3.8.0",
-        "networkx==2.3",
-        "motor==2.0.0"
+        "pymongo>=3.8.0",
+        "networkx>=2.3",
+        "motor>=2.0.0"
     ],
     include_package_data=True
 )
