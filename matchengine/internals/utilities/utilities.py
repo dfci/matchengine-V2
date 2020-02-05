2be9d5cef8b60897bb45119400bc6f44fb6564d2
esiegel <esiegel@jimmy.harvard.edu>
2020-02-05 17:19:12 -0500

    fix sorting bug

--- matchengine/internals/utilities/utilities.py
+++ matchengine/internals/utilities/utilities.py
@@ -169,7 +169,7 @@ def get_sort_order(matchengine: MatchEngine, match_document: Dict) -> list:
 
     # If an idenfitifer is not a protocol id (e.g. 17-251) then skip replacing
     identifier = match_document[matchengine.match_criteria_transform.trial_identifier]
-    if isinstance(identifier, ObjectId):
+    if isinstance(identifier, ObjectId) or identifier is None:
         pass
     else:
         sort_array.append(int(identifier.replace("-", "")))
