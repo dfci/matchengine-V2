8afa6397de004089902bd58827403b24d547026c
esiegel <esiegel@jimmy.harvard.edu>
2020-08-10 11:38:44 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="MatchEngine V2",
-    version="2.1",
+    version="2.1.1",
     packages=[
         "matchengine",
         "matchengine.internals",
@@ -15,6 +18,7 @@ setup(
     author='Eric Marriott, Ethan Siegel',
     author_email='esiegel@ds.dfci.harvard.edu',
     description='Open source engine for matching cancer patients to precision medicine clinical trials (V2).',
+    long_description=long_description,
     entrypoints={
         "console-scripts": [
             "matchengine =  matchengine.main"
@@ -29,5 +33,24 @@ setup(
         "networkx>=2.3",
         "motor>=2.0.0"
     ],
-    include_package_data=True
+    include_package_data=True,
+    python_requires='>=3.7',
+    download_url='https://github.com/dfci/matchengine-V2/archive/2.0.0.tar.gz',
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: POSIX :: BSD",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Topic :: Utilities",
+        "Typing :: Typed"
+    ]
 )
